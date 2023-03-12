fa54383c120254c8f0a7e0d388842f7a5607a293
andreweggleston <egglestonandrew927@gmail.com>
2023-03-12 12:42:25 -0400

    move car stats to their own class
    a CarStats is made up of 3 Stats: topSpeed, acceleration, and handling. All 3 Stats are enums from ONE(1) to TEN(10).
    
    CarStats is responsible for converting from the 0-10 scale to doubles that are usable by Car.

--- src/Car.java
+++ src/Car.java
@@ -3,9 +3,7 @@ import javax.swing.Icon;
 public class Car {
     private Icon icon;
 
-    private int topSpeed;
-    private int acceleration;
-    private int handling;
+    private CarStats stats;
 
     private double momentum;
     private double distanceFromLast;
@@ -15,9 +13,7 @@ public class Car {
 
     public Car(Icon icon, int topSpeed, int acceleration, int handling, Node goalNode) {
         this.icon = icon;
-        this.topSpeed = topSpeed;
-        this.acceleration = acceleration;
-        this.handling = handling;
+        this.stats = new CarStats(topSpeed, acceleration, handling);
         this.goalNode = goalNode;
         this.momentum = 0.0;
         this.distanceFromLast = 0.0;
@@ -41,9 +37,11 @@ public class Car {
 
             //do the handling "spin out" logic
             //1 handling is 50% spin out, 10 handling is 2% spin out
-            double randomChance = spinoutChance(this.handling, this.lastNode);
+            double turnAngle = Math.abs(lastNode.next().getAngle() - lastNode.getAngle()); //TODO probably wrong
+            double angleScale = turnAngle/120.0; //TODO MAGIC NUMBER
+            double spinout = stats.handling()*angleScale;
 
-            if (Math.random() < randomChance) {
+            if (Math.random() < spinout) {
                 momentum = 0.0;
             }
 
@@ -52,15 +50,79 @@ public class Car {
         }
         return false;
     }
+}
 
-    private static double spinoutChance(int handling, Node node) {
-        double turnAngle = Math.abs(node.next().getAngle() - node.getAngle()); //TODO probably wrong
-        double angleScale = turnAngle/120.0;
+class CarStats {
+    private Stat topSpeed;
+    private Stat acceleration;
+    private Stat handling;
 
-        double handlingScale = (0.53)*((10-handling)/10.0);
-        assert handlingScale > 0.0; //dont have more than 10 handling
-        double spinout = (0.02 + handlingScale)*angleScale;
+    static double TOP_SPEED_SCALE_FACTOR = 2.5;
+    static double ACCELERATION_SCALE_FACTOR = 1.0;
+    static double HANDLING_SCALE_FACTOR = 0.53;
+    static double MIN_HANDLING_FACTOR = 0.02;
 
-        return spinout;
+    public CarStats(int topSpeed, int acceleration, int handling){
+        assert (topSpeed > 0 && topSpeed < 10) && (acceleration > 0 && acceleration < 10) && (handling > 0 && handling < 10)
+        this.topSpeed = Stat.fromInt(topSpeed);
+        this.acceleration = Stat.fromInt(acceleration);
+        this.handling = Stat.fromInt(handling);
+    }
+
+    public double topSpeed() {
+        return topSpeed.statNumeral * TOP_SPEED_SCALE_FACTOR;
+    }
+
+    public double acceleration() {
+        return acceleration.statNumeral * ACCELERATION_SCALE_FACTOR;
+    }
+
+    public double handling() {
+        return (10-handling.statNumeral)/10.0 * HANDLING_SCALE_FACTOR + MIN_HANDLING_FACTOR;
+    }
+
+    enum Stat {
+        ONE(1) {
+            @Override
+            public Stat decrement() {
+                return ONE;
+            }
+        },
+        TWO(2),
+        THREE(3),
+        FOUR(4),
+        FIVE(5),
+        SIX(6),
+        SEVEN(7),
+        EIGHT(8),
+        NINE(9),
+        TEN(10) {
+            @Override
+            public Stat increment() {
+                return TEN;
+            }
+        };
+
+        private final int statNumeral;
+
+        Stat(int i) {
+            statNumeral = i;
+        }
+
+        public int getStatNumeral() {
+            return statNumeral;
+        }
+
+        public Stat increment() {
+            return values()[ordinal() + 1];
+        }
+
+        public Stat decrement() {
+            return values()[ordinal() - 1];
+        }
+
+        public static Stat fromInt(int i) {
+            return Stat.values()[i];
+        }
     }
 }
