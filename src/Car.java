0cc09bfa4a722af5599c5cb840b5dc1aa3f16995
andreweggleston <egglestonandrew927@gmail.com>
2023-03-12 23:04:40 -0400

    moved CarStats to private static inner class
    linting

--- src/Car.java
+++ src/Car.java
@@ -23,7 +23,7 @@ public class Car {
     public boolean drive(int ms) {
         boolean aboutToFinish = lastNode.next().equals(goalNode);
         momentum += Math.min(stats.topSpeed(), stats.acceleration());
-        distanceFromLast += momentum*(ms/10.0);
+        distanceFromLast += momentum * (ms / 10.0);
 
         //if the car has passed a turn
         if (distanceFromLast > lastNode.distanceToNext()) {
@@ -38,8 +38,8 @@ public class Car {
             //do the handling "spin out" logic
             //1 handling is 50% spin out, 10 handling is 2% spin out
             double turnAngle = Math.abs(lastNode.next().getAngle() - lastNode.getAngle()); //TODO probably wrong
-            double angleScale = turnAngle/120.0; //TODO MAGIC NUMBER
-            double spinout = stats.handling()*angleScale;
+            double angleScale = turnAngle / 120.0; //TODO MAGIC NUMBER
+            double spinout = stats.handling() * angleScale;
 
             if (Math.random() < spinout) {
                 momentum = 0.0;
@@ -51,82 +51,127 @@ public class Car {
         return false;
     }
 
-    public boolean isPlayer() {return true;}
-
-}
-
-class CarStats {
-    private Stat topSpeed;
-    private Stat acceleration;
-    private Stat handling;
-
-    static double TOP_SPEED_SCALE_FACTOR = 2.5;
-    static double ACCELERATION_SCALE_FACTOR = 1.0;
-    static double HANDLING_SCALE_FACTOR = 0.53;
-    static double MIN_HANDLING_FACTOR = 0.02;
-
-    public CarStats(int topSpeed, int acceleration, int handling){
-        assert (topSpeed > 0 && topSpeed < 10) && (acceleration > 0 && acceleration < 10) && (handling > 0 && handling < 10);
-        this.topSpeed = Stat.fromInt(topSpeed);
-        this.acceleration = Stat.fromInt(acceleration);
-        this.handling = Stat.fromInt(handling);
+    public boolean isPlayer() {
+        return true;
     }
 
-    public double topSpeed() {
-        return topSpeed.statNumeral * TOP_SPEED_SCALE_FACTOR;
+    public void incrementTopSpeed() {
+        this.stats.topSpeed = stats.topSpeed.increment();
     }
 
-    public double acceleration() {
-        return acceleration.statNumeral * ACCELERATION_SCALE_FACTOR;
+    public void decrementTopSpeed() {
+        this.stats.topSpeed = stats.topSpeed.decrement();
     }
 
-    public double handling() {
-        return (10-handling.statNumeral)/10.0 * HANDLING_SCALE_FACTOR + MIN_HANDLING_FACTOR;
+    public void incrementAcceleration() {
+        this.stats.acceleration = stats.acceleration.increment();
     }
 
-    enum Stat {
-        ONE(1) {
-            @Override
-            public Stat decrement() {
-                return ONE;
+    public void decrementAcceleration() {
+        this.stats.acceleration = stats.acceleration.decrement();
+    }
+
+    public void incrementHandling() {
+        this.stats.handling = stats.handling.increment();
+    }
+
+    public void decrementHandling() {
+        this.stats.handling = stats.handling.decrement();
+    }
+
+    public int getTopSpeed() {
+        return stats.topSpeed.statNumeral;
+    }
+
+    public int getAcceleration() {
+        return stats.acceleration.statNumeral;
+    }
+
+    public int getHandling() {
+        return stats.handling.statNumeral;
+    }
+
+    //TODO explain reasoning for using a private static inner class
+    private static class CarStats {
+
+
+        Stat topSpeed;
+        Stat acceleration;
+        Stat handling;
+
+        private static final double TOP_SPEED_SCALE_FACTOR = 2.5;
+        private static final double ACCELERATION_SCALE_FACTOR = 1.0;
+        private static final double HANDLING_SCALE_FACTOR = 0.53;
+        private static final double MIN_HANDLING_FACTOR = 0.02;
+
+        public CarStats(int topSpeed, int acceleration, int handling) {
+            assert (topSpeed > 0 && topSpeed < 10) && (acceleration > 0 && acceleration < 10) && (handling > 0 && handling < 10);
+            this.topSpeed = Stat.fromInt(topSpeed);
+            this.acceleration = Stat.fromInt(acceleration);
+            this.handling = Stat.fromInt(handling);
+        }
+
+        public double topSpeed() {
+            return topSpeed.statNumeral * TOP_SPEED_SCALE_FACTOR;
+        }
+
+        public double acceleration() {
+            return acceleration.statNumeral * ACCELERATION_SCALE_FACTOR;
+        }
+
+        public double handling() {
+            return (10 - handling.statNumeral) / 10.0 * HANDLING_SCALE_FACTOR + MIN_HANDLING_FACTOR;
+        }
+
+        /**
+         * Stat enumerates the integer values [1,10]. We use an enumerator because we want the rigorous type
+         * definition provided by an enum. This guarantees that a Stat won't exist with values outside the range
+         * provided by the enum.
+         * We use an enum--instead of a class, even if we want methods--because a class would not allow for this
+         * behavior without constant checking of the actual integer value of the stat. With an enum those integer values
+         * are set once when the program is compiled and then cannot be touched during runtime.
+         */
+        enum Stat {
+            ONE(1) {
+                @Override
+                public Stat decrement() {
+                    return ONE;
+                }
+            },
+            TWO(2),
+            THREE(3),
+            FOUR(4),
+            FIVE(5),
+            SIX(6),
+            SEVEN(7),
+            EIGHT(8),
+            NINE(9),
+            TEN(10) {
+                @Override
+                public Stat increment() {
+                    return TEN;
+                }
+            };
+
+            private final int statNumeral;
+
+            Stat(int i) {
+                statNumeral = i;
             }
-        },
-        TWO(2),
-        THREE(3),
-        FOUR(4),
-        FIVE(5),
-        SIX(6),
-        SEVEN(7),
-        EIGHT(8),
-        NINE(9),
-        TEN(10) {
-            @Override
+
             public Stat increment() {
-                return TEN;
+                return values()[ordinal() + 1];
             }
-        };
 
-        private final int statNumeral;
+            public Stat decrement() {
+                return values()[ordinal() - 1];
+            }
 
-        Stat(int i) {
-            statNumeral = i;
+            public static Stat fromInt(int i) {
+                return Stat.values()[i];
+            }
         }
 
-        public int getStatNumeral() {
-            return statNumeral;
-        }
-
-        public Stat increment() {
-            return values()[ordinal() + 1];
-        }
-
-        public Stat decrement() {
-            return values()[ordinal() - 1];
-        }
-
-        public static Stat fromInt(int i) {
-            return Stat.values()[i];
-        }
     }
 
 }
