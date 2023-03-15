3025643c19fb4d8587d83bdb029be8f459a82b85
The1PhoenixKing <pganzratzat@gmail.com>
2023-03-15 16:38:57 -0400

    Wrote generateCars() including comments. Created base fileReader functionality. Moved main() to GroceryGrandPrix.

--- src/GroceryGrandPrix.java
+++ src/GroceryGrandPrix.java
@@ -1,8 +1,11 @@
 // Phoenix Ganz-Ratzat
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -13,10 +16,11 @@ public class GroceryGrandPrix implements ActionListener {
     private int round;
     private int tickRate;
     private double timeElapsed;
-    private ArrayList <Car> cars = new ArrayList<Car>();
-    private JButton[] buttons;
     private GUI gui;
     private Node head;
+    private ArrayList <Car> cars ;
+    private JButton[] buttons;
+    private ImageIcon[] icons;
     public GroceryGrandPrix() {
         generateNodes(5);
         hurried = false;
@@ -25,6 +29,7 @@ public class GroceryGrandPrix implements ActionListener {
         round = 1;
         tickRate = 33;
         timeElapsed = 0;
+        cars = new ArrayList<Car>();
         generateCars();
         createButtons();
         gui = new GUI(Color.WHITE, buttons);
@@ -33,13 +38,19 @@ public class GroceryGrandPrix implements ActionListener {
 
     public void startGame() {
         gui.playerMenu(round, budget);
+        for (int i = 0; i < cars.size(); i++) {
+            gui.createPreview(i);
+        }
     }
 
+    private void openMenu() {
 
+    }
 
     private void showTrack() {
-        generateNodes(round*4);
+        generateNodes(round*10);
         gui.buildSegments(head);
+        gui.drawTrack();
     }
 
     private void simulateRace() {
@@ -87,8 +98,65 @@ public class GroceryGrandPrix implements ActionListener {
         // reset the game to round 1
     }
 
+    private void fileReader() throws IOException {
+        File[] spriteFiles = (new File("sprites")).listFiles();
+        icons = new ImageIcon[spriteFiles.length];
+
+        for (int i = 0; i < spriteFiles.length; i++) {
+            icons[i] = new ImageIcon(ImageIO.read(spriteFiles[i]));
+        }
+    }
+
+
     private void generateCars() {
-        // create array of cars
+        Random rand = new Random();
+        Car car;
+        Node temp = head;
+        int iconIndex;
+        double statPicker;
+        int maxStat = 10;
+        int statPoints = 10;
+        // The number at which stat2's lower bound becomes 1 after which the upper bound will begin to decrease.
+        int boundOffset = (statPoints - maxStat) - 1;
+        int stat1;
+        int stat2;
+        int stat3;
+
+
+        for (int i = 0; i < 4; i++) {
+            if (i == 0) {
+                car = new Car(icons[0], 5, 5, 5, temp);
+            }
+            // Generate 3 random stat numbers, then create a car with a random stat number passed in for each of the cars stats.
+            else {
+                // Determines which stat variable will be passed to each Car stat.
+                statPicker = Math.random();
+                /* Generates a number from 1 to the maximum number of points that can be allocated to any stat, inclusive.
+                   If the stat points allocated across all stats is less than the stat maximum, stat points - 2 is used instead to ensure each stat gets at least 1 point. */
+                stat1 = rand.nextInt(Math.min(maxStat, statPoints - 2)) + 1;
+                /* Generates a number with an upper and lower bound dependent on stat1, with the minimum being 1 and the maximum being equal to maxStat.
+                   If stat1 only leaves 4 stats to be divided between the other two stats this stat could have any value from 1-3.
+                   If stat1 leaves 20 stats this will always equal 10. */
+                stat2 = rand.nextInt(maxStat - Math.abs((boundOffset) - stat1)) + Math.max(1, (boundOffset + 1) - stat1);
+                // Assigns the remaining unallocated stat points to the last stat.
+                stat3 = (statPoints - stat1) - stat2;
+
+                iconIndex = rand.nextInt(icons.length);
+
+                // Randomizes the order in which each of the stats are passed to Car's constructor to offset any bias towards each stat.
+                if (statPicker > (2.0/3.0)) {
+                    car = new Car(icons[iconIndex], stat1, (statPicker >= (5.0/6.0)) ? stat2 : stat3,  (statPicker >= (5.0/6.0)) ? stat3 : stat2, temp);
+                }
+                else if (statPicker >= (1.0/3.0)) {
+                    car = new Car(icons[iconIndex], stat2, (statPicker >= .5) ? stat1 : stat3,  (statPicker >= .5) ? stat3 : stat1, temp);
+                }
+                else {
+                    car = new Car(icons[iconIndex], stat3, (statPicker > (1.0/6.0)) ? stat1 : stat2,  (statPicker > (1.0/6.0)) ? stat2 : stat1, temp);
+                }
+            }
+            cars.set(i, car);
+            temp = temp.next();
+        }
     }
 
     private void generateNodes(int number) {
@@ -177,6 +245,10 @@ public class GroceryGrandPrix implements ActionListener {
         restart.addActionListener(this);
         restart.setActionCommand("restart");
         buttons = new JButton[] {plus1, plus2, plus3, minus1, minus2, minus3, startRace, hurry, pause, restart};
+    }
+
+    public static void main(String[] args) {
+        GroceryGrandPrix test = new GroceryGrandPrix();
     }
 
 }
--- src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        GroceryGrandPrix test = new GroceryGrandPrix();
-    }
-}
